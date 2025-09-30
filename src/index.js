091680b0fb1a6a556b85b636ec8f334e7398d185
prashantpstae-arch <prashantpstae@gmail.com>
2025-09-30 11:47:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,31 @@
 const { prisma } = require("../db/config");
 
 async function createUserWithPost({ name, email, title, content }) {
+  if (!name || !email || !title || !content) {
+    return { success: false };
+  }
 
-// Write your code here
+  try {
+    await prisma.$transaction(async (tx) => {
+      const user = await tx.user.create({
+        data: { name, email },
+      });
 
-if (!name || !email || !title || !content) {
+      await tx.post.create({
+        data: {
+          title,
+          content,
+          userId: user.id,
+        },
+      });
+    });
 
-return { success: false };
-
-}
-
-try {
-
-await prisma.$transaction (async (tx) => {
-
-const user = await tx.user.create({ data: { name, email } });
-
-await tx.post.create({ data: { content, title, userId: user.id
-
-});
-
-return { success: true };
-
-} catch (err) {
-
-console.log(err);
-
-return { success: false };
-
-}
-
+    // If transaction succeeds
+    return { success: true };
+  } catch (err) {
+    console.error(err);
+    return { success: false };
+  }
 }
 
 module.exports = { createUserWithPost };
