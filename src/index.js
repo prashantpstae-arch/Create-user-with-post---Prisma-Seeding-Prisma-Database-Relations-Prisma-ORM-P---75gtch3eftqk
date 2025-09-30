a38e6eeeb4b3a9456692720b2991c405bb07a870
prashantpstae-arch <prashantpstae@gmail.com>
2025-09-30 10:55:44 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const { prisma } = require("../db/config");
+
+async function createUserWithPost({ name, email, title, content }) {
+  // Write your code here
+}
+
+module.exports = { createUserWithPost };
