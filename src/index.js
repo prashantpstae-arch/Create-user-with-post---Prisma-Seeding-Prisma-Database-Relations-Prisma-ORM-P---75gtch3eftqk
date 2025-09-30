dd655b669afdc2d1391cc1c79d033dc024f0a99d
prashantpstae-arch <prashantpstae@gmail.com>
2025-09-30 11:43:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 const { prisma } = require("../db/config");
 
 async function createUserWithPost({ name, email, title, content }) {
-  // Write your code here
+
+// Write your code here
+
+if (!name || !email || !title || !content) {
+
+return { success: false };
+
+}
+
+try {
+
+await prisma.$transaction (async (tx) => {
+
+const user = await tx.user.create({ data: { name, email } });
+
+await tx.post.create({ data: { content, title, userId: user.id
+
+});
+
+return { success: true };
+
+} catch (err) {
+
+console.log(err);
+
+return { success: false };
+
+}
+
 }
 
 module.exports = { createUserWithPost };
